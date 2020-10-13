0185c4dd77cecac3fa02b706db9796ee163106d6
Alec <alecautry20181@gmail.com>
2020-10-13 14:13:40 -0500

    Create main.py
    Starter main file for program

---
+++ main.py
@@ -0,0 +1,8 @@
+class Dominoe():
+    def __init__(self, isTrump, isDouble, High, Low, ID, Points):
+    self.isTrump = False
+    self.isDouble = True
+    self.High = 0
+    self.Low = 0
+    self.ID = 0
+    self.Points = 0
