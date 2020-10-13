16d892194d59c49ee299653e1152d885e9e8875d
Alec <alecautry20181@gmail.com>
2020-10-13 14:40:53 -0500

    Update main.py
    Fixed the class

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class Dominoe():
     def __init__(self, isTrump, isDouble, High, Low, ID, Points):
-    self.isTrump = False
-    self.isDouble = True
-    self.High = 0
-    self.Low = 0
-    self.ID = 0
-    self.Points = 0
+    self.isTrump = isTrump    # Value to see if a trump
+    self.isDouble = isDouble  # Value to test for a double
+    self.High = High          # The large side of the dominoe
+    self.Low = Low            # The small side of the dominoe
+    self.ID = ID              # The ID representing domonies in an order
+    self.Points = .25         # The value of the dominoe when adding for tricks
