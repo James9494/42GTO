5018266d4761f4a68219cb0b1fef678e43786e28
Alec <alecautry20181@gmail.com>
2020-10-13 14:47:29 -0500

    Update main.py
    Fixed class agiain, added test code for creating 1 domino

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-class Dominoe():
+class Domino():
     def __init__(self, isTrump, isDouble, High, Low, ID, Points):
     self.isTrump = isTrump    # Value to see if a trump
     self.isDouble = isDouble  # Value to test for a double
     self.High = High          # The large side of the dominoe
     self.Low = Low            # The small side of the dominoe
     self.ID = ID              # The ID representing domonies in an order
-    self.Points = .25         # The value of the dominoe when adding for tricks
+    self.Points = Points      # The value of the dominoe when adding for tricks
+    
+ D0 = Domino()
+ D0.isTrump = False
+ D0.isDouble = True
+ D0.High = 0
+ D0.Low = 0
+ D0.ID = 0
+ self.Points = .25
+ print(D0.High)
