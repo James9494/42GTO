ff0f653b4a2e8f1a7171675bcaa05739193f92da
Alec <alecautry20181@gmail.com>
2020-10-13 14:59:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 class Domino():
-    def __init__(self, isTrump, isDouble, High, Low, ID, Points):
-    self.isTrump = isTrump    # Value to see if a trump
-    self.isDouble = isDouble  # Value to test for a double
-    self.High = High          # The large side of the dominoe
-    self.Low = Low            # The small side of the dominoe
-    self.ID = ID              # The ID representing domonies in an order
-    self.Points = Points      # The value of the dominoe when adding for tricks
-    
+	def __init__(self, isTrump, isDouble, High, Low, ID, Points):
+	self.isTrump = isTrump    # Value to see if a trump
+	self.isDouble = isDouble  # Value to test for a double
+	self.High = High          # The large side of the dominoe
+	self.Low = Low            # The small side of the dominoe
+	self.ID = ID              # The ID representing domonies in an order
+	self.Points = Points      # The value of the dominoe when adding for tricks
+
  D0 = Domino()
  D0.isTrump = False
  D0.isDouble = True
