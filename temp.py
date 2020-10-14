0a61c7a763e958e570bd18cf836f7f44e7534e7f
Alec <alecautry20181@gmail.com>
2020-10-13 22:33:36 -0500

    Update temp.py
    I am the best

--- temp.py
+++ temp.py
@@ -1,1 +1,24 @@
-#create any function here you want to send while not messing with main.py
+create any function here you want to send while not messing with main.py
+
+HandCreation Function     *** Based on the lead domino
+Use ^^^ function above to create each hand at each phase of game
+
+
+def CreateHand(suit, hand):
+	AmountOf = 0
+	DTemp = Domino(False, True, 0, 0, 0, .25)
+	temp = 0
+	for i in range(len(hand)):
+		if hand[i].High == Suit or hand[i].Low == suit:
+			AmountOf += 1
+	if AmountOf = 0:
+		tempArray[len(hand)]
+		for i in range len(hand):
+			tempArray[i] = hand[i]
+	else:
+		tempArray[AmountOf]
+		for i in range(AmountOf):
+		if hand[i].High == Suit or hand[i].Low == suit:
+			tempArray[temp] = hand[i]
+			temp +=1
+	
